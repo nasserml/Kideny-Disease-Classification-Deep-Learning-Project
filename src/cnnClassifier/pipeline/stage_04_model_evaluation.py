bfb83eb6bf44774fb307b47059d54863fb337cb4
Mahmoud Nasser <mnasserone@gmail.com>
2023-10-23 06:40:54 +0300

    dvc added

--- src/cnnClassifier/pipeline/stage_04_model_evaluation.py
+++ src/cnnClassifier/pipeline/stage_04_model_evaluation.py
@@ -35,7 +35,7 @@ class EvaluationPipeline:
         evaluation = Evaluation(eval_config)
         evaluation.evaluation()
         evaluation.save_score()
-        evaluation.log_into_mlflow()
+        #evaluation.log_into_mlflow()
 
 
 
